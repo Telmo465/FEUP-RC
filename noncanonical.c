d509a248f18316c6bc6d52b756ca5deb838648b4
Telmo Botelho <30441457+Telmo465@users.noreply.github.com>
2021-11-05 17:59:53 +0000

    Add files via upload

--- noncanonical.c
+++ noncanonical.c
@@ -1,99 +1,32 @@
-/*Non-Canonical Input Processing*/
+/** 
+ * Non-Canonical Input Processing
+ * From https://tldp.org/HOWTO/Serial-Programming-HOWTO/x115.html by Gary Frerking and Peter Baumann
+**/
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <strings.h>
+
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #define BAUDRATE B9600
-#define MODEMDEVICE "/dev/ttyS1"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
-#define MAX_BUF_SIZE 255
 #define FALSE 0
 #define TRUE 1
 
+#define FLAG 0x7E
+#define A_ER 0x03 // resposta do recetor
+#define A_RE 0x01 // resposta do emissor
+#define C_UA 0x03
+#define BCC(a,c) (a^c)
+
+
+
 volatile int STOP = FALSE;
-
-int write_noncanonical(char *serialPort, char *str)
-{
-  int fd, c, res;
-  struct termios oldtio, newtio;
-  char buf[255];
-  int i, sum = 0, speed = 0;
-
-  if ((strcmp("/dev/ttyS0", serialPort) != 0))
-  {
-    printf("Bad serial port\n");
-    exit(1);
-  }
-
-  /*
-    Open serial port device for reading and writing and not as controlling tty
-    because we don't want to get killed if linenoise sends CTRL-C.
-  */
-
-  fd = open(serialPort, O_RDWR | O_NOCTTY);
-  if (fd < 0)
-  {
-    perror(serialPort);
-    exit(-1);
-  }
-
-  if (tcgetattr(fd, &oldtio) == -1)
-  { /* save current port settings */
-    perror("tcgetattr");
-    exit(-1);
-  }
-
-  bzero(&newtio, sizeof(newtio));
-  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-  newtio.c_iflag = IGNPAR;
-  newtio.c_oflag = 0;
-
-  /* set input mode (non-canonical, no echo,...) */
-  newtio.c_lflag = 0;
-
-  newtio.c_cc[VTIME] = 0; /* inter-character timer unused */
-  newtio.c_cc[VMIN] = 1;  /* blocking read until 5 chars received */
-
-  /* 
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-    leitura do(s) pr�ximo(s) caracter(es)
-  */
-
-  tcflush(fd, TCIOFLUSH);
-
-  if (tcsetattr(fd, TCSANOW, &newtio) == -1)
-  {
-    perror("tcsetattr");
-    exit(-1);
-  }
-
-  printf("New termios structure set\n");
-
-  res = write(fd, str, 255);
-  printf("%d bytes written\n", res);
-
-  /* 
-    O ciclo FOR e as instru��es seguintes devem ser alterados de modo a respeitar 
-    o indicado no gui�o 
-  */
-
-  if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
-  {
-    perror("tcsetattr");
-    exit(-1);
-  }
-
-  close(fd);
-  return 0;
-}
-
 
 int main(int argc, char **argv)
 {
@@ -101,12 +34,11 @@ int main(int argc, char **argv)
   struct termios oldtio, newtio;
   char buf[255];
 
-
   if ((argc < 2) ||
       ((strcmp("/dev/ttyS0", argv[1]) != 0) &&
-       (strcmp("/dev/ttyS4", argv[1]) != 0)))
+       (strcmp("/dev/ttyS1", argv[1]) != 0)))
   {
-    printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS4\n");
+    printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
     exit(1);
   }
 
@@ -127,6 +59,8 @@ int main(int argc, char **argv)
     perror("tcgetattr");
     exit(-1);
   }
+
+
 
   bzero(&newtio, sizeof(newtio));
   newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
@@ -153,24 +87,38 @@ int main(int argc, char **argv)
   }
 
   printf("New termios structure set\n");
-  char strRead[MAX_BUF_SIZE];
-  int n = 0;
 
-  while (STOP == FALSE)
-  {                           /* loop for input */
-    res = read(fd, buf, 1);   /* returns after 1 char has been input */
-    buf[res] = 0;             /* so we can printf... */
-    strRead[n] = buf[res];
-    n++;
-    printf(":%s:%d\n", buf, res);
-    if (buf[0] == '\0')
-      STOP = TRUE;
-  }
-  write_noncanonical("/dev/ttyS0", strRead);
+
+
+  char UA[10];
+
+  UA[0] = FLAG;
+  UA[1] = A_ER;
+  UA[2] = C_UA;
+  UA[3] = BCC(A_ER,C_UA);
+  UA[4] = FLAG;
+
+
+
+
 
   /* 
     O ciclo WHILE deve ser alterado de modo a respeitar o indicado no gui�o 
   */
+  while (STOP == FALSE)
+  {                           /* loop for input */
+    res = read(fd, buf, 1); /* returns after 5 chars have been input */
+    buf[res] = 0;             /* so we can printf... */
+    //printf(":%s:%d\n", buf, res);
+    //printf("adswdaw");
+    if (buf[0] == 'z')
+      STOP = TRUE;
+  } 
+
+  printf("UA: %s%d", buf, 6);
+
+  res = write(fd, UA, 255);
+  //printf("%d bytes written\n", res);
 
   sleep(1);
   tcsetattr(fd, TCSANOW, &oldtio);
